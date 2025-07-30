03f8bf5ec2057f699b6c48ed51b03d8c5a0ef338
Patsy Bárcena <patsybarcena783@gmail.com>
2025-07-30 23:00:10 +0000

    Remove extra root index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todolist.pugdev/', // 
+  base: '/todolist.pugdev/', // 👈 esto es fundamental
 })
