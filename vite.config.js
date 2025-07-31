96cd1d58432567b0424bb3d9288e7e914022cda1
Patsy Bárcena <patsybarcena783@gmail.com>
2025-07-30 22:56:19 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todolist.pugdev/', // 👈 esto es fundamental
+  base: './', // 👈 esta línea es clave
 })
